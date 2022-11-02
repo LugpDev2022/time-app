46b9306f60b2d6b17c1fce4657cbb0537645e057
LugpDev2022 <lugpdev2022@gmail.com>
2022-11-02 09:26:56 -0600

    Simplify the useLocalClock hook

--- src/hooks/useLocalClock.jsx
+++ src/hooks/useLocalClock.jsx
@@ -6,15 +6,13 @@ export const useLocalClock = () => {
     minutes: new Date().getMinutes().toString(),
   });
 
-  useEffect(() => {
-    if (hours.length < 2) {
-      setTime({ minutes, hours: `0${hours}` });
-    }
+  if (hours.length < 2) {
+    setTime({ minutes, hours: '0' + hours });
+  }
 
-    if (minutes.length < 2) {
-      setTime({ minutes: `0${minutes}`, hours });
-    }
-  }, []);
+  if (minutes.length < 2) {
+    setTime({ minutes: '0' + minutes, hours });
+  }
 
   useEffect(() => {
     setInterval(() => {
@@ -22,24 +20,8 @@ export const useLocalClock = () => {
       const newMinutes = date.getMinutes().toString();
       const newHours = date.getHours().toString();
 
-      if (minutes !== newMinutes && newMinutes.length < 2) {
-        if (newHours.length < 2) {
-          setTime({ hours: `0${newHours}`, minutes: `0${newMinutes}` });
-          return;
-        }
-
-        setTime({ hours: newHours, minutes: `0${newMinutes}` });
-        return;
-      }
-
       if (minutes !== newMinutes) {
-        if (newHours.length < 2) {
-          setTime({ hours: `0${newHours}`, minutes: `0${newMinutes}` });
-          return;
-        }
-
         setTime({ hours: newHours, minutes: newMinutes });
-        return;
       }
     }, 5000);
   }, []);
