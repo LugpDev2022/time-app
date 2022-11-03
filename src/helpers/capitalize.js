a4ae04cc8e1b99a051b1e6245487303d41eb2d1a
LugpDev2022 <lugpdev2022@gmail.com>
2022-11-03 13:49:30 -0600

    Capitalize the city on submit form

--- src/helpers/capitalize.js
+++ src/helpers/capitalize.js
@@ -2,7 +2,7 @@ export const capitalize = sentence => {
   const words = sentence.split(' ');
 
   for (let i = 0; i < words.length; i++) {
-    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1).toLowerCase();
   }
 
   return words.join(' ');
--- src/pages/worldClock/hooks/useCountryForm.jsx
+++ src/pages/worldClock/hooks/useCountryForm.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { capitalize } from '../../../helpers/capitalize';
 import { addClock, handleError } from '../../../store/slices/worldClock';
 
 export const useCountryForm = () => {
@@ -26,8 +27,9 @@ export const useCountryForm = () => {
     if (city.length >= 1) {
       const action = {
         region: regionSelect.current.value,
-        city,
+        city: capitalize(city),
       };
+
       dispatch(addClock(action));
       setCity('');
       return;
