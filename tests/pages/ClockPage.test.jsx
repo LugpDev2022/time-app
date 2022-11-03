a81cf7b1fc82cf16f99430792bd53605d862d360
LugpDev2022 <lugpdev2022@gmail.com>
2022-11-03 16:17:50 -0600

    Tests on ClockPage.jsx

---
+++ tests/pages/ClockPage.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import { ClockPage } from '../../src/pages';
+import { useLocalClock } from '../../src/hooks';
+
+jest.mock('../../src/hooks/useLocalClock');
+
+describe('tests on <ClockPage />', () => {
+  test('should match snapshot', () => {
+    useLocalClock.mockReturnValue({
+      hours: '05',
+      minutes: '07',
+    });
+
+    const { container } = render(<ClockPage />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should show correctly the clock', () => {
+    useLocalClock.mockReturnValue({
+      hours: '05',
+      minutes: '07',
+    });
+
+    render(<ClockPage />);
+    expect(screen.getByText('05:07'));
+  });
+});
