e6d45d0e06f433fa34f4101bfac28610dfa90509
LugpDev2022 <lugpdev2022@gmail.com>
2022-11-03 21:17:25 -0600

    Make the decrementTime action

--- src/store/slices/timer/timerSlice.js
+++ src/store/slices/timer/timerSlice.js
@@ -4,17 +4,40 @@ export const timerSlice = createSlice({
   name: 'timer',
   initialState: {
     counter: {
-      minutes: '10',
+      minutes: '1',
       seconds: '00',
     },
     isRunning: false,
+    isFinished: false,
   },
   reducers: {
     toggleRunnig: state => {
       state.isRunning = !state.isRunning;
     },
     decrementTime: state => {
-      console.log('decrementTime');
+      const secondsInNumber = parseInt(state.counter.seconds);
+
+      if (
+        parseInt(state.counter.minutes) === 0 &&
+        parseInt(state.counter.seconds) === 0
+      ) {
+        state.isRunning = false;
+        state.isFinished = true;
+        return;
+      }
+
+      if (secondsInNumber === 0) {
+        state.counter.minutes = parseInt(state.counter.minutes) - 1;
+        state.counter.seconds = '59';
+        return;
+      }
+
+      if (secondsInNumber < 10) {
+        state.counter.seconds = '0' + (secondsInNumber - 1).toString();
+        return;
+      }
+
+      state.counter.seconds = (secondsInNumber - 1).toString();
     },
   },
 });
