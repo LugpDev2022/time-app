e722a8f2bacb0ef84b9e2f459deaf25c4503f3e4
LugpDev2022 <lugpdev2022@gmail.com>
2022-11-18 16:33:19 -0600

    Disable accept button when new timer is on 0:00

--- src/pages/timer/views/NewTimerView.jsx
+++ src/pages/timer/views/NewTimerView.jsx
@@ -1,8 +1,20 @@
-import { Button, ButtonGroup, Col, Container, Row } from 'react-bootstrap';
-import { BigNumber, Box, Title } from '../../../components';
-import { ExitButton } from '../components';
+import React, { useEffect, useState } from "react";
+import { Button, ButtonGroup, Col, Container, Row } from "react-bootstrap";
+import { BigNumber, Box, Title } from "../../../components";
+import { ExitButton } from "../components";
 
 export const NewTimerView = () => {
+  const newTimer = "0:00";
+  const [isCorrect, setIsCorrect] = useState();
+
+  useEffect(() => {
+    if (newTimer !== "0:00") {
+      setIsCorrect(false);
+      return;
+    }
+    setIsCorrect(true);
+  }, [newTimer]);
+
   return (
     <Box>
       <Container>
@@ -12,16 +24,18 @@ export const NewTimerView = () => {
           <Title>New Timer</Title>
         </Row>
         <Row>
-          <div className='d-flex align-items-center justify-content-center'>
-            <BigNumber>00:00</BigNumber>
+          <div className="d-flex align-items-center justify-content-center">
+            <BigNumber>{newTimer}</BigNumber>
           </div>
         </Row>
-        <Row className='justify-content-center'>
+        <Row className="justify-content-center">
           <Col xs={12} sm={8} md={6} xl={5}>
-            <ButtonGroup aria-label='Timer buttons' className='mb-3 w-100'>
-              <Button className='p-2'>+</Button>
-              <Button className='p-2'>Accept</Button>
-              <Button className='p-2'>-</Button>
+            <ButtonGroup aria-label="Timer buttons" className="mb-3 w-100">
+              <Button className="p-2">+</Button>
+              <Button className="p-2" disabled={isCorrect}>
+                Accept
+              </Button>
+              <Button className="p-2">-</Button>
             </ButtonGroup>
           </Col>
         </Row>
