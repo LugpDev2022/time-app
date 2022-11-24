7e6f0fe230d0ad7873c275ae812407750711b948
LugpDev2022 <lugpdev2022@gmail.com>
2022-11-24 13:14:49 -0600

    Create a README

---
+++ README.md
@@ -0,0 +1,40 @@
+# Time App
+
+Small app for basic time tasks like watch the hour and create timers.
+
+## Screenshots
+
+![Screenshot](https://user-images.githubusercontent.com/97978467/203851877-2f50ae9b-228d-4650-8f65-c26e31fc73c3.png)
+
+## Demo
+
+https://time-app-lugpdev2022.netlify.app/
+
+## Tech Stack
+
+- [React](https://es.reactjs.org/)
+- [React Bootstrap](https://react-bootstrap.github.io/)
+- [Yarn](https://yarnpkg.com/)
+- [Vite](https://vitejs.dev/)
+- [Styled components](https://styled-components.com/)
+- [Redux Toolkit](https://redux-toolkit.js.org/)
+- [Sass](https://sass-lang.com/)
+
+### For testing
+- [Jest](https://jestjs.io/)
+- [React Testing Library](https://testing-library.com/)
+
+## Installation
+
+Clone the repository with git
+
+```bash
+    git clone https://github.com/LugpDev2022/time-app.git
+```
+
+Install the dependencies using yarn
+
+```bash
+    cd time-app
+    yarn
+```
--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,16 +1,17 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { NavbarBrand, NavbarLink } from './';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavbarBrand, NavbarLink } from "./";
 
 export const AppNavbar = () => (
-  <Navbar expand='md' bg='dark' variant='dark'>
+  //TODO: Add world clock page
+  <Navbar expand="md" bg="dark" variant="dark">
     <Container>
       <NavbarBrand />
-      <Navbar.Toggle aria-controls='navbar-nav' />
-      <Navbar.Collapse id='navbar-nav'>
+      <Navbar.Toggle aria-controls="navbar-nav" />
+      <Navbar.Collapse id="navbar-nav">
         <Nav>
-          <NavbarLink to='/'>Clock</NavbarLink>
-          <NavbarLink to='/world-clock'>World Clock</NavbarLink>
-          <NavbarLink to='/timer'>Timer</NavbarLink>
+          <NavbarLink to="/">Clock</NavbarLink>
+          <NavbarLink to="/timer">Timer</NavbarLink>
         </Nav>
       </Navbar.Collapse>
     </Container>
